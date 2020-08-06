9a31c2fad931a5ff2ed1190cb563ab4aa040081d
jose <josecheng92@163.com>
2020-08-06 22:06:42 +0800

    add banner

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,16 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
-class HomePage extends StatefulWidget{
+class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage>{
+class _HomePageState extends State<HomePage> {
+  List _imageUrls = [
+    'http://pages.ctrip.com/commerce/promote/20180718/yxzy/img/640sygd.jpg',
+    'https://dimg04.c-ctrip.com/images/700u0r000000gxvb93E54_810_235_85.jpg',
+    'https://dimg04.c-ctrip.com/images/700c10000000pdili7D8B_780_235_57.jpg'
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('首页'),
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 160,
+              child: Swiper(
+                itemCount: _imageUrls.length,
+                autoplay: true,
+                itemBuilder: (BuildContext context, int index) {
+                  return Image.network(
+                    _imageUrls[index],
+                    fit: BoxFit.fill,
+                  );
+                },
+                pagination: SwiperPagination(),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
